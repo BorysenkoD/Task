3620c5c5c5f58fa34c27feb2327ab19d1aee83b0
BorysenkoD <borysenkodmitry@gmail.com>
2020-08-31 21:44:15 +0300

    6

--- main.js
+++ main.js
@@ -2,4 +2,8 @@ function change_color(elem, color) {
  elem.split(',').forEach(function(el){
     document.getElementById(el).style.background = color;
  }) 
-} 
+};
+
+window.onload = function(){
+	document.body.style.backgroundColor = "white";
+};  
